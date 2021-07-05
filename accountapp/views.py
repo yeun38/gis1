4c3dca47666567eb773efb6c03c624228f1e4400
gitct <choh1997@naver.om>
2021-07-05 14:33:46 +0900

    7/5 1ban class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,5 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def hello_world(args):
+    pass
+
+
 def hello(request):
-    return HttpResponse("hello")
+    return render(request , 'accountapp/hello_world.html')
