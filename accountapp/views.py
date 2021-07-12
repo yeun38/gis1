4bd964e7f146ab5e86980d7505546a814cc91e37
gitct <choh1997@naver.om>
2021-07-12 15:11:05 +0900

    7/5 1ban class commit 1.1

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
@@ -14,10 +16,9 @@ def hello(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        hello_world_list = HelloWorld.objects.all()
+        return HttpResponseRedirect(reverse('accountapp:hello'))
+    # redirect to - 어디로 재연결할건지 , reverse -역추적을통해 위치를 알려줌.
 
-        return render(request , 'accountapp/hello_world.html',
-                      context={'hello_world_list': hello_world_list})
     else :
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
