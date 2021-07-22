1f837ef4bea1d33d19d69f18176ad6d1cd8cbcec
gitct <choh1997@naver.om>
2021-07-22 15:14:28 +0900

    7/22 1ban class commit 2

--- pracitce03/settings.py
+++ pracitce03/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'accountapp',
+    'profileapp',
 
 ]
 
@@ -135,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
