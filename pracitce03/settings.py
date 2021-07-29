c184039ca8a4559ffe5fb0acb49a686e44984918
gitct <choh1997@naver.om>
2021-07-29 15:17:46 +0900

    7/29 1ban class commit 2

--- pracitce03/settings.py
+++ pracitce03/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'bootstrap4',
     'accountapp',
     'profileapp',
+    'articleapp',
 
 ]
 
--- pracitce03/urls.py
+++ pracitce03/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/',admin.site.urls),
     path('accounts/',include('accountapp.urls')),
     path('profiles/',include('profileapp.urls')),
+    path('articles/',include('profileapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
