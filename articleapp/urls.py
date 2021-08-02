ce31cad62ed089c4f114d16396e7087872c5ba9f
gitct <choh1997@naver.om>
2021-08-02 16:37:09 +0900

    8/02 1ban class commit 5

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -8,11 +8,9 @@ app_name = 'articleapp'
 urlpatterns = [
     path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
 
-    path('create/', ArticleCreateView.as_view(), name= 'create')
+    path('create/', ArticleCreateView.as_view(), name= 'create'),
 
     path('detail/<int:pk>', ArticleDetailView.as_view(), name = 'detail'),
-
-
 
 
 ]
