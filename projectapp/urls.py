4272d1c20c9cdfd71e4dc49c90817c2da0910105
gitct <choh1997@naver.om>
2021-08-19 17:11:49 +0900

    8/18 1ban class commit 4

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -5,8 +5,8 @@ from projectapp.views import ProjectCreateView, ProjectDetailView, ProjectListVi
 app_name = 'projectapp'
 
 urlpatterns = [
-    path('create/', ProjectCreateView().as_view(), name='create'),
-    path('detail/<int:pk>', ProjectDetailView().as_view(), name='detail'),
+    path('create/', ProjectCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
 
     path('list/', ProjectListView.as_view(), name='list'),
 
