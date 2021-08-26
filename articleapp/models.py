27d043fdf91bbe92d976d8e510628f9fc58dce5a
gitct <choh1997@naver.om>
2021-08-26 16:38:15 +0900

    8/26 1ban class commit 4

--- articleapp/models.py
+++ articleapp/models.py
@@ -9,10 +9,10 @@ class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL,
                                related_name='article', null=True)
     project = models.ForeignKey(Project, on_delete=models.SET_NULL,
-                                related_name='article', null=True)
+                                related_name='article', null=True, blank=True)
 
 
-    title = models.CharField(max_length=200,null=True)
+    title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)
     content = models.TextField(null=True)
 
