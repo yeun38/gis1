b600df17137f15de587db42e12539e957ffc5353
gitct <choh1997@naver.om>
2021-09-09 16:04:52 +0900

    09/09 1ban class commit 2

---
+++ pracitce03/settings/local.py
@@ -0,0 +1,31 @@
+from .base import *
+
+
+env_list = dict()
+local_env = open(os.path.join(BASE_DIR, '.env'))
+while True:
+    line = local_env.readline()
+    if not line:
+        break
+    line = line.replace('\n', '')
+    start = line.find('=')
+    key = line[:start]
+    value = line[start+1:]
+    env_list[key] = value
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env_list['SECRET_KEY']
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ["*"]
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
