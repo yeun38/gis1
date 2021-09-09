ec97c7e1faede21e1e6173a5d4c3bf6f429a9d94
gitct <choh1997@naver.om>
2021-09-09 16:52:07 +0900

    09/09 1ban class commit 2

--- pracitce03/settings/deploy.py
+++ pracitce03/settings/deploy.py
@@ -1,7 +1,10 @@
 from .base import *
 
+
 env_list = dict()
+
 local_env = open(os.path.join(BASE_DIR, '.env'))
+
 while True:
     line = local_env.readline()
     if not line:
@@ -11,11 +14,12 @@ while True:
     key = line[:start]
     value = line[start+1:]
     env_list[key] = value
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env_list['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -28,7 +32,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': 'pass1234',
+        'PASSWORD': 'password1234',
         'HOST': 'mariadb',
         'PORT': '3306',
     }
--- pracitce03/settings/local.py
+++ pracitce03/settings/local.py
@@ -2,7 +2,9 @@ from .base import *
 
 
 env_list = dict()
+
 local_env = open(os.path.join(BASE_DIR, '.env'))
+
 while True:
     line = local_env.readline()
     if not line:
@@ -17,11 +19,13 @@ while True:
 SECRET_KEY = env_list['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
 
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
